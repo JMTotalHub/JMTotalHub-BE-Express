57589565ec239b5f0bf9d513ce56aa75b048e14c
junmop95 <junmop950@gmail.com>
2024-07-19 16:58:31 +0900

    로그인 기능 구현 중

--- domains/auth/services/auth.service.js
+++ domains/auth/services/auth.service.js
@@ -15,12 +15,12 @@ export async function signUpUser(bodyData) {
 export async function signInUser(bodyData) {
 
     // 비밀번호 해쉬화 (복잡도 10~12이 권고)
-    const { password } = bodyData;
+    const { email, password } = bodyData;
     const hashedPassword = await bcrypt.hash(password, 10);
-
     
+    const user = AuthRepository.findUserByEmail(email);
 
     bodyData.password = hashedPassword;
 
-    return await AuthRepository.insertUser(bodyData);
+    return await AuthRepository.findUserByEmail(email);
 }
